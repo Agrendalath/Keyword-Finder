c4adc9b58657d5df6ac09d93e297e11a436ff9e8
Agrendalath <Agrendalath@gmail.com>
2017-02-18 18:52:00 +0100

    Initial commit.

---
+++ keyword_finder/keyword_finder/celeryconf.py
@@ -0,0 +1,12 @@
+import os
+
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keyword_finder.settings')
+app = Celery('keyword_finder')
+
+CELERY_TIME_ZONE = 'UTC'
+
+app.config_from_object('django.conf:settings')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
---
+++ run_web.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sleep 4 &&
+    cd keyword_finder &&
+    su -m finder -c "./manage.py makemigrations keyword_finder" &&
+    su -m finder -c "./manage.py migrate" &&
+    su -m finder -c "./manage.py runserver 0.0.0.0:8000"
